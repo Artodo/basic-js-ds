d18bf35a8af3ccf9b9bd1433701af30d13c726bb
Artur Dyshynski <tavebran@gmail.com>
2023-04-17 15:52:59 +0300

    feat: init

---
+++ src/queue.js
@@ -0,0 +1,64 @@
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    getUnderlyingList() {
+        return {
+            value: this.head.value,
+            next: this.head.next
+        };
+    }
+
+    enqueue(value) {
+        const node = new Node(value);
+
+        if (this.head) {
+            this.tail.next = node;
+            this.tail = node;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
+
+        this.length++;
+    }
+
+    dequeue() {
+        const current = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        return current.value;
+    }
+}
+
+module.exports = {
+    Queue
+};
---
+++ src/stack.js
@@ -0,0 +1,58 @@
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+class Node {
+    constructor(element1) {
+        this.element = element1;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    push(element) {
+        const node = new Node(element);
+        if (this.head) {
+            node.next = this.head;
+            this.head = node;
+        } else {
+            this.head = node;
+        }
+        this.length++;
+    }
+
+
+    pop() {
+        const current = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        return current.element;
+    }
+
+
+    peek() {
+        return this.head.element;
+    }
+
+
+}
+
+
+
+module.exports = {
+    Stack
+};
